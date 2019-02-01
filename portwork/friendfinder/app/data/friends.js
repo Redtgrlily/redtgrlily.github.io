64ffdae06d6195da39b91788ace1e533cf5d02ea
redtgrlily <redtgrlily@outlook.com>
2019-02-01 12:00:59 -0600

    friend finder

---
+++ portwork/friendfinder/app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Adri',
+		photo: 'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/37561322_10156676619504048_6177245599759335424_o.jpg?_nc_cat=100&_nc_oc=AQkMSm7A1DKKFxboJ6AKsTm21orobUpguBmsfCDOGJ0UJyI-82z6FbH6hz6Vbkk6xQ0&_nc_ht=scontent-dfw5-2.xx&oh=52f263281a7526b5f20614c804ec41ac&oe=5CF84870',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'JoVi',
+		photo: 'https://thenypost.files.wordpress.com/2017/08/114172693cm002_bon_jovi_in_.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Harry Potter',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/Harry_Potter_character_poster.jpg/220px-Harry_Potter_character_poster.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Hermoine',
+		photo: 'https://images.ctfassets.net/bxd3o8b291gf/4CqtYU9oOQagGaoWYaeSiM/4c1c7aef842c16a6c7ca407212cb6ee2/WB_F6_HermioneWithButterbeerOnUpperLip_HP6D-09923r.jpg?w=1200',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jinny',
+		photo: 'http://en.bcdn.biz/Images/2018/6/6/6c869cfe-3294-4706-ab2e-1ba98dec747b.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Bambi',
+		photo: 'https://images.news18.com/ibnlive/uploads/2018/12/Bambi.jpeg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Johnny Depp',
+		photo: 'https://a57.foxnews.com/a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2018/09/640/320/1862/1048/jack-sparrow-ap.jpg?ve=1&tl=1?ve=1&tl=1',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Angry Cat',
+		photo: 'https://pbs.twimg.com/profile_images/2888549903/9e4be2724ccad6e66a9858db2fce9457.jpeg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mike',
+		photo: 'https://yt3.ggpht.com/a-/AAuE7mBbZRBWDubpSgqQ8cCiJyR-D1bX8VHa4fPvKQ=s900-mo-c-c0xffffffff-rj-k-no',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Winnie Pooh',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Winniethepooh.png/220px-Winniethepooh.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
